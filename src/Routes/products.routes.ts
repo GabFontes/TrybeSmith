272a2e9fae2105c498cc2124fded12cfb8782a2b
gab <gabrielfontes321@gmail.com>
2022-05-13 17:57:18 -0300

    feat: implementing post product route

--- src/Routes/products.routes.ts
+++ src/Routes/products.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/ProductsController';
+import { validateAmount, validateName } from '../middlewares/productValidation';
 
 const ProductsRouter = Router();
 
 ProductsRouter.get('/', ProductsController.list);
+ProductsRouter.post('/', validateName, validateAmount, ProductsController.create);
 
 export default ProductsRouter;
