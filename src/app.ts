bb69f5a4a940cd9a4efe5303643c66d965194640
gab <gabrielfontes321@gmail.com>
2022-05-13 19:54:26 -0300

    feat: calling the user router in the app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import errorHandler from './middlewares/errorHandler';
 import ProductsRouter from './Routes/products.routes';
+import UsersRouter from './Routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', ProductsRouter);
+app.use('/users', UsersRouter);
 app.use(errorHandler);
 
 export default app;
