4c954def543a88f777f455462120b15d31fb8d44
gab <gabrielfontes321@gmail.com>
2022-05-13 19:52:56 -0300

    feat: adding next() function typing

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import IError from '../Interfaces/IError';
 
-export default (error: IError, _req: Request, res: Response) => {
+export default (error: IError, _req: Request, res: Response, _next: NextFunction) => {
   console.log(error);
   return res.status(500).send({ message: 'Something went wrong' });
 };
